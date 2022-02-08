397933c2cf92a85d75be07b6cf13d6dfc877f456
Cullen <cullenjcasey@gmail.com>
2022-02-07 21:02:03 -0800

    words can change map

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,32 +1,69 @@
 var fact = 'https://cat-fact.herokuapp.com/facts';
+var factDisplay = document.getElementById("catFact");
+var userLat;
+var userLng;
+var map;
 
 /**
  * Returns coordinates for a 3 word address
  * @param {string} words - The 3 word address words to convert to coordinates
  * @returns {Promise} - Promise coordinates response
  */
-what3words.api.convertToCoordinates("filled.count.soap")
+what3words.api.convertToCoordinates("tugging.owls.racked")
     .then(function (response) {
         console.log(response.coordinates);
+        userLat = parseFloat(response.coordinates.lat);
+        userLng = parseFloat(response.coordinates.lng);
+        console.log(userLng, userLat);
+
+        map = L.map('map', {
+            center: [userLat, userLng],
+            zoom: 13
+        });
+
+        // create map
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWx0b2lkaWFuIiwiYSI6ImNrejZoNnVmMTBrajUyb3FucHFyYmZ5b3oifQ.7vJzATafyBzcbJ4K14w7Ow', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'your.mapbox.access.token'
+        }).addTo(map);
+
     });
 
 
-    
-// initialize the map on the "map" div with a given center and zoom
-var map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 13
+document.getElementById("startW3W").addEventListener("click", function () {
+    var userInput = document.getElementById("words").value;
+    console.log(userInput);
+    document.getElementById("instructions").textContent = "";
+    what3words.api.convertToCoordinates(userInput)
+        .then(function (response) {
+            console.log(response.coordinates);
+            map.setView(response.coordinates);
+})
+        .catch(function (error) {
+            console.log(error.message);
+            document.getElementById("instructions").textContent=error.message;
+        })
 });
 
-// create map
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWx0b2lkaWFuIiwiYSI6ImNrejZoNnVmMTBrajUyb3FucHFyYmZ5b3oifQ.7vJzATafyBzcbJ4K14w7Ow', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(map);
+// initialize the map on the "map" div with a given center and zoom
+// var map = L.map('map', {
+//     center: [userLng, userLat],
+//     zoom: 13
+// });
+
+// // create map
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWx0b2lkaWFuIiwiYSI6ImNrejZoNnVmMTBrajUyb3FucHFyYmZ5b3oifQ.7vJzATafyBzcbJ4K14w7Ow', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'your.mapbox.access.token'
+// }).addTo(map);
 
 
 //generate a random number
@@ -43,5 +80,5 @@ fetch(fact)
     })
     .then(function (data) {
         // console.log(data)
-        console.log(data[RNG(0, 4)].text)
+        factDisplay.textContent = data[RNG(0, 4)].text
     })
